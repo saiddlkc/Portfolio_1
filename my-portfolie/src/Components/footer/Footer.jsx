0ce957c130410645bf3ba182af3c5c0403a5f1d2
saiddlkc <saidalkilic@gmail.com>
2024-01-28 22:53:50 +0100

    d

--- my-portfolie/src/Components/footer/Footer.jsx
+++ my-portfolie/src/Components/footer/Footer.jsx
@@ -9,22 +9,22 @@ const Footer = () => {
       </a>
       <ul className="permalinks">
         <li>
-          <a href="#">Home</a>
+          <a href="">Home</a>
         </li>
         <li>
-          <a href="#about"></a>About
+          <a href="#about">About</a>
         </li>
         <li>
-          <a href="#experience"></a>Experience
+          <a href="#experience">Experience</a>
         </li>
         <li>
-          <a href="#portfolio"></a>Portfolio
+          <a href="#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="#testimonials"></a>Testimonials
+          <a href="#testimonials">Testimonials</a>
         </li>
         <li>
-          <a href="#contact"></a>Contact
+          <a href="#contact">Contact</a>
         </li>
       </ul>
       <div className="footer__socials">
