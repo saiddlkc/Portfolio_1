2764e8e31c899b41ee0a5b754fda6ad9deaf8e9e
saiddlkc <saidalkilic@gmail.com>
2024-01-28 22:45:28 +0100

    d

--- my-portfolie/src/Components/contact/Contact.jsx
+++ my-portfolie/src/Components/contact/Contact.jsx
@@ -1,8 +1,69 @@
 import React from "react";
 import "./contact.css";
+import { IoMdMail } from "react-icons/io";
+import { FaDiscord } from "react-icons/fa6";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <IoMdMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>saidalkilic@mail.com</h5>
+            <a href="mailto:saidalkilic@mail.com"> Send a message</a>
+          </article>
+          <div className="contact__options">
+            <article className="contact__option">
+              <FaDiscord className="contact__option-icon" />
+              <h4>Discord</h4>
+              <h5>said_dlkc</h5>
+              <a href="https://discord.com/" target="_blank">
+                add me on Discord
+              </a>
+            </article>
+          </div>
+          <div className="contact__options">
+            <article className="contact__option">
+              <FaDiscord className="contact__option-icon" />
+              <h4>Discord</h4>
+              <h5>said_dlkc</h5>
+              <a href="https://discord.com/" target="_blank">
+                add me on Discord
+              </a>
+            </article>
+          </div>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full Name"
+            required
+          />
+          <input
+            type="text"
+            name="email
+          "
+            placeholder="Your Email"
+            required
+          />
+          <textarea
+            name="messae"
+            rows="10"
+            placeholder="Your Message  "
+            required
+          ></textarea>
+          <button type="submit" className="cnt btn btn-primary">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
-// https://www.youtube.com/watch?v=G-Cr00UYokU    2:39:52
+
 export default Contact;
--- my-portfolie/src/Components/footer/Footer.jsx
+++ my-portfolie/src/Components/footer/Footer.jsx
@@ -2,7 +2,42 @@ import React from "react";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Said Dlkc
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about"></a>About
+        </li>
+        <li>
+          <a href="#experience"></a>Experience
+        </li>
+        <li>
+          <a href="#portfolio"></a>Portfolio
+        </li>
+        <li>
+          <a href="#testimonials"></a>Testimonials
+        </li>
+        <li>
+          <a href="#contact"></a>Contact
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/">Instagram</a>
+        <a href="https://www.linkedin.com/">LinkedIn</a>
+        <a href="https://www.X.com/">X</a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Said Dlkc All rights open for everyone.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
