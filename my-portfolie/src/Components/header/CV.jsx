09e7d4f7c748e8c3a1cce5061c2d3e0eccb1be1d
saiddlkc <saidalkilic@gmail.com>
2024-02-02 18:08:44 +0100

    cv fertig

--- my-portfolie/src/Components/header/CV.jsx
+++ my-portfolie/src/Components/header/CV.jsx
@@ -1,3 +1,4 @@
+// CV.jsx
 import React from "react";
 import "./CV.css";
 
@@ -5,36 +6,44 @@ const CV = () => {
   return (
     <div>
       <h1>Lebenslauf</h1>
-      <small>Said Ali Dalkilic</small>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-        nisi, libero, magni aspernatur repellendus assumenda deleniti
-        exercitationem velit odit nihil neque ea, consequuntur quaerat similique
-        est non labore? Delectus placeat quae nostrum eligendi vel porro ipsa
-        non voluptates perspiciatis a aspernatur omnis eum minima quaerat sunt
-        quis, earum aut eveniet, distinctio, modi itaque magni neque suscipit.
-        Porro perferendis dolores corporis quo explicabo debitis tempore placeat
-        dicta! Est eum laboriosam, obcaecati accusantium magnam qui, facere ad
-        odio fugiat fuga ut tempora vero. Ipsa maiores quae dolores
-        exercitationem expedita dolorum eligendi doloribus molestiae voluptates?
-        Nam possimus distinctio voluptatem libero quod rem quas. Lorem ipsum
-        dolor sit, amet consectetur adipisicing elit. Quae laboriosam voluptas
-        inventore voluptate ad officiis ut similique earum impedit animi quasi
-        eligendi, facilis hic quis eum ducimus. Enim distinctio consequuntur
-        nisi beatae, iste tenetur ipsum vitae natus vero autem commodi minus
-        iure explicabo, libero hic dolor sint et officia provident soluta!
-        Alias, commodi fuga veniam pariatur modi aliquid provident ea maxime
-        numquam repudiandae. Dolor reprehenderit non saepe neque cumque ea
-        dolores consequatur enim accusamus error, accusantium sunt magnam
-        tempore molestiae blanditiis modi mollitia, officia, est sit. Illum
-        doloremque autem esse suscipit officia ipsam, iusto qui expedita
-        praesentium nesciunt! Culpa, aliquid. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Explicabo sunt quisquam atque sed expedita
-        laboriosam eveniet numquam. Earum ullam rerum consectetur exercitationem
-        magnam, nisi deleniti minima architecto vero culpa, nobis nesciunt
-        delectus officiis eos accusamus laboriosam fuga quae quibusdam vel odio
-        illo dolorem. Eligendi ea a maxime fugiat enim aut!
-      </p>
+      <div>
+        <div className="maincontainer">
+          <div className="mini-container">
+            <h3>Ausbildung</h3>
+            <ul>
+              <li>
+                09/2012 - 07/2017: -Fachhochschulreife Hermann-Hesse-Gymnasium
+              </li>
+              <li>
+                04/2020 - 09/2021: Informations- und Kommunikationstechnik HTW
+              </li>
+              <li>05/2023 - 06/2023: Erprobungscenter für Digitale Berufe</li>
+              <li>07/2023-07/2024: Fullstack Web Developer DCI</li>
+            </ul>
+          </div>
+
+          <div className="mini-container">
+            <h3>Berufserfahrung</h3>
+            <ul>
+              <li>03/2017-11/2017: Emnitel Callcenter (Marktforschung)</li>
+              <li>12/2017 - 03/2018: Datenerfasser/Support bei Zalando S.E.</li>
+              <li>
+                03/2018 - 03/2019: Content Managment/HelpDesk bei Zalando S.E.
+              </li>
+              <li>05/2019 - 12/2019: Dispatcher bei Amazon Logistics</li>
+              <li>04/2020-03/2023: Gastronomie/Kurier bei Lieferando</li>
+            </ul>
+          </div>
+
+          <div className="mini-container">
+            <h3>Praktika</h3>
+            <ul>
+              <li>06/2014-08/2014: Projex IT-Handel und Technik</li>
+              <li>08/17-07/18: Zuerich-Hort Grundschule FSJ</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
